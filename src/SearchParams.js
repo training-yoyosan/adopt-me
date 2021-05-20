dc9474812516d08b131c36aa23f78ca2919ed4b9
YoloSan <veselu@gmail.com>
2021-05-20 16:54:17 +0300

    add custom hook

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Pet from "./Pet";
+import useBreedList from "./useBreed";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
-const BREEDS = [];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
+  const [breeds] = useBreedList(animal);
+
+  useEffect(
+    () => {
+      async function requestPets() {
+        const res = await fetch(
+          `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+        );
+
+        const json = await res.json();
+
+        setPets(json.pets);
+      }
+
+      requestPets();
+    },
+    [] // eslint-disable-line react-hooks/exhaustive-deps
+    // [] - call the API once
+    // [animal] - will call the API every time animal's value changes
+    // if there's no parameter give, it will do infinite calls
+  );
 
   return (
     <div className="search-params">
@@ -45,7 +68,7 @@ const SearchParams = () => {
             onBlur={(e) => setBreed(e.target.value)}
           >
             <option />
-            {BREEDS.map((breed, index) => (
+            {breeds.map((breed, index) => (
               <option value={breed} key={index}>
                 {breed}
               </option>
@@ -54,6 +77,14 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+        />
+      ))}
     </div>
   );
 };
