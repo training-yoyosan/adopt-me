98232f0febe430458aff786ff804886b825ba739
YoloSan <veselu@gmail.com>
2021-05-20 19:51:14 +0300

    use Link tag

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import ReactDOM from "react-dom";
 import { React, StrictMode } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
       <Router>
+        <header>
+          <Link to="/">
+            <h1>Adopt Me!</h1>
+          </Link>
+        </header>
         <Switch>
           <Route path="/details/:id">
             <Details />
