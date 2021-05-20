8a74da6c8479162fb0630bba2788ee4fd266288b
YoloSan <veselu@gmail.com>
2021-05-20 17:33:08 +0300

    component composition: move results in a separate component

---
+++ src/Results.js
@@ -0,0 +1,26 @@
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  {
+    return (
+      <div className="search">
+        {!pets.length ? (
+          <h2>No pets found</h2>
+        ) : (
+          pets.map((pet) => (
+            <Pet
+              name={pet.name}
+              animal={pet.animal}
+              breed={pet.breed}
+              key={pet.id}
+              id={pet.id}
+              location={`${pet.city}, ${pet.state}`}
+            />
+          ))
+        )}
+      </div>
+    );
+  }
+};
+
+export default Results;
