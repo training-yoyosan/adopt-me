eacd618064d24d4b064c1b49f421cd54fbe340a5
YoloSan <veselu@gmail.com>
2021-05-20 19:46:11 +0300

    use react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import ReactDOM from "react-dom";
 import { React, StrictMode } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
---
+++ src/Details.js
@@ -0,0 +1,5 @@
+const Details = () => {
+  return <h2>hi lolol omg wtf!</h2>;
+};
+
+export default Details;
