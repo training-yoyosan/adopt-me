18a5e7481ebd8f81daa4e76c5d89007281bba2dc
YoloSan <veselu@gmail.com>
2021-05-20 18:04:27 +0300

    set all links to use https

--- src/Pet.js
+++ src/Pet.js
@@ -1,5 +1,5 @@
 const Pet = ({ name, animal, breed, images, location, id }) => {
-  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  let hero = "https://pets-images.dev-apis.com/pets/none.jpg";
   if (images.length) {
     hero = images[0];
   }
--- src/useBreed.js
+++ src/useBreed.js
@@ -19,7 +19,7 @@ export default function useBreedList(animal) {
       setBreedList([]);
       setStatus("loading");
       const res = await fetch(
-        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+        `https://pets-v2.dev-apis.com/breeds?animal=${animal}`
       );
       const json = await res.json();
       localCache[animal] = json.breeds || [];
