0d5b956dfbc620a0a80d44ea2c42233fe51c177d
YoloSan <veselu@gmail.com>
2021-05-21 12:42:21 +0300

    more complex state management in a class component

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
+import Carousel from "./Carousel";
 
 class Details extends Component {
   // allowed by using @babel/plugin-proposal-class-properties
@@ -21,10 +22,12 @@ class Details extends Component {
       return <h2 style={{ textAlign: "center" }}>loading ...</h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
 
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>
