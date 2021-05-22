b3e76d8a01f921aea781c5b582862f6e04daeb1b
YoloSan <veselu@gmail.com>
2021-05-22 20:59:31 +0300

    theme context: add extra comment

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -2,6 +2,7 @@ import { createContext } from "react";
 
 // the parameter is a default value we aren't going to call
 // but it's useful to signify the type for Typescript
+// the second element in the array is the setState method of the state hook.
 const ThemeContext = createContext(["green", () => {}]);
 
 export default ThemeContext;
